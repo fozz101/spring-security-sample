145a35b8c2b4c49a89e8f3b3639d01406071d7f6
fozz101 <fedi.galfat@esprit.tn>
2022-08-03 03:56:35 +0100

    testing spring security

---
+++ src/main/java/io/fozz101/springsecuritysample/SpringSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package io.fozz101.springsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fozz101/springsecuritysample/SpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fozz101.springsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
